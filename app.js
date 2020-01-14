35d01d0ce2262d5adbffecddc17b07f2233bc785
Josh <jocoso@users.noreply.github.com>
2020-01-14 11:28:20 -0500

    Added basic error handling

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
 
 // Card Routes
 const charactersRoutes = require("./api/routes/characters");
@@ -14,5 +17,21 @@ app.use("/techs", techsRoutes);
 app.use("/places", placesRoutes);
 app.use("/drugs", drugsRoutes);
 app.use("/chances", chancesRoutes);
+
+// Error Handling
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status(404);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
