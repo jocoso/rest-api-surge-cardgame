cb1846ec9ea06314186eef6ca2e2016d0324abbc
Josh <jocoso@users.noreply.github.com>
2020-01-16 11:28:27 -0500

    Deleted the Schemas inside models files

--- api/models/faction.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const factionSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String, required: true }
-});
-
-module.exports = mongoose.model("Faction", factionSchema);
--- api/models/weapon-type.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const weaponTypeSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String, required: true }
-});
-
-module.exports = mongoose.model("WeaponType", weaponTypeSchema);
