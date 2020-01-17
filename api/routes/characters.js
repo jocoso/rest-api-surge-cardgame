3208037db2d92fbc5964ef3b9ee2a3893e0d97cd
Josh <jocoso@users.noreply.github.com>
2020-01-17 14:09:12 -0500

    Added requests for POST PATCH DELETE and GET

--- api/routes/characters.js
+++ api/routes/characters.js
@@ -7,7 +7,11 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   pool.query("SELECT * FROM characters", (err, results) => {
     if (err) res.status(500).json({ error: err });
-    res.status(200).json(results.rows);
+
+    res.status(200).json({
+      count: results.rows.length,
+      characters: results.rows
+    });
   });
 });
 
@@ -15,55 +19,103 @@ router.get("/", (req, res, next) => {
 router.get("/:characterId", (req, res, next) => {
   const id = req.params.characterId;
 
-  if (id === "special") {
-    res.status(200).json({
-      message: "You have discovered the special ID",
-      id: id
-    });
-  } else {
-    res.status(200).json({
-      message: "You passed an ID"
-    });
-  }
-});
+  const query = {
+    // give the query a unique name
+    name: "fetch-character",
+    text: "SELECT * FROM characters WHERE id = $1",
+    values: [id]
+  };
 
-// Handle GET request to /characters/{characterId}/info
-router.get("/:characterId/info", (req, res, next) => {
-  res.status(200).json({
-    message: "info will all be here"
+  pool.query(query, (err, response) => {
+    if (err) return res.status(500).json({ error: err });
+
+    // If character exists
+    if (response.rowCount) {
+      res.status(200).json(response.rows);
+    } else {
+      return res.status(404).json({ message: "Character not found" });
+    }
   });
 });
 
 // Handle POST request to /characters
 router.post("/", (req, res, next) => {
   // Security Measure
+  // TODO: Convert into middleware
   if (!req.header("apiKey") || req.header("apiKey") !== process.env.API_KEY) {
     return res.status(401).json({ status: "error", message: "Unauthorized." });
   }
-  // ...
 
   const { name } = req.body;
+  // ...
+  // Heavy lifting
+  const query = {
+    text: "INSERT INTO characters(name) VALUES($1)",
+    values: [name]
+  };
 
-  pool.query("INSERT INTO characters(name) VALUES ($1)", [name]),
-    err => {
-      if (err) return res.status(500).json({ error: err });
-    };
-
-  res.status(201).json({ status: "Success", message: "Character added" });
+  // promise
+  pool
+    .query(query)
+    .then(response => {
+      console.log(response.rows[0]);
+      res.status(201).json({
+        message: "Character created sucessfully",
+        data: response.rows[0]
+      });
+    })
+    .catch(err => {
+      console.error(err.stack);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 // Handle PATCH requests to /characters/{characterId}
 router.patch("/:characterId", (req, res, next) => {
-  res.status(200).json({
-    message: "Updated Character!"
-  });
+  let query = "update characters set ";
+  const values = [];
+  const prop = req.body;
+
+  for (let i = 0; i < prop.length; i++) {
+    query += prop[i].propName + "=$" + (i + 1);
+
+    query += i < prop.length - 1 ? "," : " ";
+
+    values[i] = prop[i].value;
+  }
+
+  query += "where id=$" + (prop.length + 1);
+  values[prop.length] = req.params.characterId;
+
+  console.log(query);
+
+  pool
+    .query(query, values)
+    .then(response => {
+      res.status(200).json({ message: "Character updated sucessfully" });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 // Handle DELETE requests to /characters/{characterId}
 router.delete("/:characterId", (req, res, next) => {
-  res.status(200).json({
-    message: "Deleted Character!"
-  });
+  const id = parseInt(req.params.characterId);
+
+  pool
+    .query("delete from characters where id = $1", [id])
+    .then(result => {
+      res.status(200).json({
+        status: "success"
+      });
+    })
+    .catch(function(err) {
+      return res.status(500).json(err);
+    });
 });
 
 module.exports = router;
