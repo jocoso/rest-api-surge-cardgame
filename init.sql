59697279af694442f13d91b191f80f66a5e8e991
Josh <jocoso@users.noreply.github.com>
2020-01-17 11:20:45 -0500

    Deployed API

--- init.sql
+++ init.sql
@@ -2,10 +2,10 @@ CREATE TABLE characters
 (
     ID SERIAL PRIMARY KEY,
     name varchar(30) NOT NULL
-)
+);
 
 INSERT INTO characters
     (name)
 VALUES
     ('Priest'),
-    ('Miriam')
+    ('Miriam');
